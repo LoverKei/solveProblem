f000b3cc7db5dde20e8e6d43ead8eb5f51938d8c
LoverKei <loverkeii@gmail.com>
2017-03-08 10:59:25 +0900

    Create L4_1_PermCheck.py

---
+++ codility/L4_1_PermCheck.py
@@ -0,0 +1,14 @@
+def solution(A):
+  # find Duplicate
+  # find out of range(N)
+  
+  N = len(A)
+  
+  B = [False] * N
+  
+  for i in A:
+    if if > N or B[i-1]:
+      return 0
+    B[i-1] = True
+    
+  return 1
